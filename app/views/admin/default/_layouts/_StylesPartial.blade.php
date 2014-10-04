836486ca17302e3d17754126013fe50077001243
Subodh Dahal <contact.subodhdahal@gmail.com>
2014-10-04 22:20:09 +0545

    Changed the way styles are loaded in admin section

--- app/views/admin/default/_layouts/_StylesPartial.blade.php
+++ app/views/admin/default/_layouts/_StylesPartial.blade.php
@@ -1,25 +1,26 @@
-<link href="{{url("assets/admin/default/css/bootstrap.css")}}" rel="stylesheet">
-<link href="{{url("assets/admin/default/css/jquery.gritter.css")}}" rel="stylesheet">
-<link href="{{url("assets/admin/default/css/bootstrap-responsive.css")}}" rel="stylesheet">
-<link rel="stylesheet" href="{{url("assets/admin/default/css/font-awesome.css")}}">
-<link href="{{url("assets/admin/default/css/chosen.css")}}" rel="stylesheet">
+{{ HTML::style('assets/admin/default/css/bootstrap.css') }}
+{{ HTML::style('assets/admin/default/css/jquery.gritter.css') }}
+{{ HTML::style('assets/admin/default/css/bootstrap-responsive.css') }}
+{{ HTML::style('assets/admin/default/css/font-awesome.css') }}
+{{ HTML::style('assets/admin/default/css/chosen.css') }}
 <!--[if IE 7]>
-<link rel="stylesheet" href="{{url("assets/admin/default/css/font-awesome-ie7.min.css")}}">
+{{ HTML::style('assets/admin/default/css/font-awesome-ie7.min.css') }}
 <![endif]-->
-<link href="{{url("assets/admin/default/css/styles.css")}}" rel="stylesheet">
+{{ HTML::style('assets/admin/default/css/styles.css') }}
 
-<link href="{{url("assets/admin/default/css/theme-blue.css")}}" rel="stylesheet">
+{{ HTML::style('assets/admin/default/css/theme-blue.css') }}
 
 <!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="{{url("assets/admin/default/css/ie/ie7.css")}}" />
+{{ HTML::style('assets/admin/default/css/ie/ie7.css') }}
 <![endif]-->
 <!--[if IE 8]>
-<link rel="stylesheet" type="text/css" href="{{url("assets/admin/default/css/ie/ie8.css")}}" />
+{{ HTML::style('assets/admin/default/css/ie/ie8.css') }}
 <![endif]-->
 <!--[if IE 9]>
-<link rel="stylesheet" type="text/css" href="{{url("assets/admin/default/css/ie/ie9.css")}}" />
+{{ HTML::style('assets/admin/default/css/ie/ie9.css') }}
 <![endif]-->
 <link href='http://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
+
 @section('styles')
     {{-- Here goes the page level styles --}}
 @show
