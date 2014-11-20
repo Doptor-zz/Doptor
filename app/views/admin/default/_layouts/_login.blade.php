c669ce18fc674be70b80bf16dcf4c50f9efcb99b
DOPTOR CMS & EMS <Doptor@users.noreply.github.com>
2014-11-20 21:52:15 +0800

    Update _login.blade.php

--- app/views/admin/default/_layouts/_login.blade.php
+++ app/views/admin/default/_layouts/_login.blade.php
@@ -5,7 +5,7 @@
 <title>{{ $title }} :: {{ trans('cms.admin_dashboard') }}</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="Admin Panel Template">
-<meta name="author" content="Westilian: Kamrujaman Shohel">
+<meta name="author" content="doptor.org">
 <!-- styles -->
 <link href="{{url("assets/admin/default/css/bootstrap.css")}}" rel="stylesheet">
 <link href="{{url("assets/admin/default/css/bootstrap-responsive.css")}}" rel="stylesheet">
