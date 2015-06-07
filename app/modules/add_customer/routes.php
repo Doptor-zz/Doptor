4b77b17948249047e96f5aafd69021cacf8a0f3d
Masud Rana <masud@doptor.org>
2015-06-07 22:24:55 +0545

    Delete everything in preparation for Laravel 5.

--- app/modules/add_customer/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$current_dir = basename(__DIR__);
-$current_module = "\\" . Str::title(basename(__DIR__));
-
-// Backend routes
-Route::resource('admin/modules/'.$current_dir, 'AddCustomerBackendController');
-Route::resource('backend/modules/'.$current_dir, 'AddCustomerBackendController');
-
--- app/views/admin/default/_layouts/_FooterPartial.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="copyright">
-    <p>
-        {{ Setting::value('footer_text') }}
-    </p>
-</div>
-<div class="scroll-top">
-    <a href="#" class="tip-top" title="Go Top"><i class="icon-double-angle-up"></i></a>
-</div>
