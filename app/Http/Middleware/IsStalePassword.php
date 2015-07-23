55bc478c8aa47b986771c8671f29991e2d18493f
Masud Rana <masud@doptor.org>
2015-07-23 17:15:24 +0545

    Bugfix: Askes to change password on first login when sample data is installed

--- app/Http/Middleware/IsStalePassword.php
+++ app/Http/Middleware/IsStalePassword.php
@@ -18,7 +18,12 @@ class IsStalePassword {
     {
         $last_pw_changed = new Carbon(current_user()->last_pw_changed);
 
-        if (Carbon::now()->diffInDays($last_pw_changed) > 180) {
+        if (
+            // has it been 180 days since this account is created
+            Carbon::now()->diffInDays(current_user()->created_at) >= 180 &&
+            // has it been 180 days since last password change
+            Carbon::now()->diffInDays($last_pw_changed) > 180
+        ) {
             return Redirect::to(Request::segment(1) . '/users/change-password')
                             ->with('error_message', 'It has been more than 6 months since you last changed your password. You need to change it before you can log in.');
         }
