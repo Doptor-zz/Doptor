84e5d3469c7b46d1cd89df74b78c337ba09268b2
Masud Rana <masud@doptor.org>
2015-08-04 23:59:05 +0545

    Add company_id to users table

---
+++ database/migrations/2015_08_03_202211_add_company_id_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCompanyIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->string('company_id')->nullable()->after('last_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('company_id');
+        });
+    }
+}
