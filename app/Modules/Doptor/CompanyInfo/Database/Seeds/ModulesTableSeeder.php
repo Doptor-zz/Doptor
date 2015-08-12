5578b961bfd39ec865629893d81f1e1aab17d5a7
Masud Rana <masud@doptor.org>
2015-08-12 17:21:27 +0545

    Added seeds for the CompanyInfo module

--- app/Modules/Doptor/CompanyInfo/Database/Seeds/ModulesTableSeeder.php
+++ app/Modules/Doptor/CompanyInfo/Database/Seeds/ModulesTableSeeder.php
@@ -18,7 +18,9 @@ class ModulesTableSeeder extends Seeder
             'vendor' => 'Doptor',
             'website' => 'http://doptor.net',
             'description' => NULL,
+            'form_fields' => '[{"name": "Name", "reg_no": "Registration Number", "logo": "Logo", "address": "Address", "website": "Website", "phone": "Phone", "email": "Email", "address": "Address"}, {"name": "Name", "reg_no": "Registration Number", "address": "Address", "website": "Website", "phone": "Phone", "fax": "Fax", "mobile": "Mobile", "email": "Email"} ]',
             'target' => 'public|admin|backend',
+            'models' => '{"Company": "Companies", "CompanyBranch": "Company Branches"}',
             'links' => '',
             'migrations' => '["2015_07_20_190240_create_mdl_doptor_incharges_table","2015_07_20_201633_create_mdl_doptor_countries_table","2015_07_20_203952_create_mdl_doptor_companies_table","2015_07_21_205437_create_mdl_doptor_company_branches_table"]',
             'enabled' => '1'
