f81d2572773c0fb05e514f4b73dfcc34c187162f
Masud Rana <masud@doptor.org>
2016-05-11 22:14:20 +0545

    Added new seed data for post and post categories

--- app/Components/posts/database/seeds/CategoriesTableSeeder.php
+++ app/Components/posts/database/seeds/CategoriesTableSeeder.php
@@ -6,34 +6,34 @@ class CategoriesTableSeeder extends Seeder {
 
 	public function run()
 	{
-		// Uncomment the below to wipe the table clean before populating
-		DB::table('categories')->delete();
+		\DB::table('categories')->delete();
 
-		$categories = array(
-            array(
-                'name' => 'First Category',
-                'alias' => 'first-category',
-                'type' => 'post',
-                'description' => 'Lorem Ipsum',
-                'status' => 'published',
-                'created_by' => 1,
-                'created_at' => date('Y-m-d'),
-                'updated_at' => date('Y-m-d')
-            ),
-            array(
-                'name' => 'Another Category',
-                'alias' => 'another-category',
-                'type' => 'page',
-                'description' => 'Lorem Ipsum',
-                'status' => 'published',
-                'created_by' => 1,
-                'created_at' => date('Y-m-d'),
-                'updated_at' => date('Y-m-d')
-            )
-		);
-
-		// Uncomment the below to run the seeder
-		DB::table('categories')->insert($categories);
+		\DB::table('categories')->insert(array (
+			array (
+				'id' => '1',
+				'name' => 'First Category',
+				'alias' => 'first-category',
+				'type' => 'post',
+				'description' => 'Lorem Ipsum',
+				'status' => 'published',
+				'created_by' => '1',
+				'updated_by' => NULL,
+				'created_at' => '2014-03-29 21:45:00',
+				'updated_at' => '2014-03-29 21:45:00',
+			),
+			array (
+				'id' => '2',
+				'name' => 'Another Category',
+				'alias' => 'another-category',
+				'type' => 'page',
+				'description' => 'Lorem Ipsum',
+				'status' => 'published',
+				'created_by' => '1',
+				'updated_by' => NULL,
+				'created_at' => '2014-03-29 21:45:00',
+				'updated_at' => '2014-03-29 21:45:00',
+			),
+		));
 	}
 
 }
